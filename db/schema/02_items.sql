db94dcfa0f021eaa3715478e01f776ebe56fb7ef
cdmac94 <collinmacleod79@gmail.com>
2022-01-09 00:11:37 +0000

    Added missing semicolons

--- db/schema/02_items.sql
+++ db/schema/02_items.sql
@@ -4,15 +4,15 @@ DROP TABLE IF EXISTS categories CASCADE;
 CREATE TABLE categories (
   id SERIAL PRIMARY KEY NOT NULL,
   category_type VARCHAR(255) NOT NULL
-)
+);
 
 DROP TABLE IF EXISTS items CASCADE;
 CREATE TABLE items (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
-  category_id INTEGER REFERENCES categories,
+  category_id INTEGER REFERENCES categories(id),
   title VARCHAR(255) NOT NULL,
-  date_created TIMESTAMP NOT NULL,
+  date_created TIMESTAMP,
   -- reminder_time DATE,
   active BOOLEAN DEFAULT TRUE
 );
