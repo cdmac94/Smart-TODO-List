4fe41b760eec9f3d8335344dcc6970dfd3406f73
JLMaynardDesign <JLMaynardDesign@gmail.com>
2022-01-12 22:07:56 -0500

    installed bcrypt, and require bcrypt in user-login related functions in routes and routes/helpers

--- routes/helpers/user_login.js
+++ routes/helpers/user_login.js
@@ -1,24 +1,16 @@
-//no cookies involved
-//not sure how to go about this.
-/*const user = findUserByEmail(email, users);
-const findUserByEmail = (email, database) => {
-  for (const property in database) {
-    const user = database[property];
-    if (user.email === email) {
-      return user;
-    }
-  }
-  return null;
-}; */
+const bcrypt = require('bcrypt');
 
-
-const userLogin = function (email, password, db) {
-
-  const userLoginQuery = `SELECT FROM users WHERE email = $1 AND password = $2`;
-  const value = [email, password];
-
+const userLogin = function(email, password, db) {
+  const userLoginQuery = `SELECT FROM users WHERE email = $1`;
+  const value = [email];
   return db.query(userLoginQuery, value)
     .then(res => res.rows[0])
+    .then(res => {
+      if (bcrypt.compareSync(password, res.password) && res !== undefined) {
+        return res;
+      }
+      return null;
+    })
     .catch(err => {
       console.log(err);
     });
--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,16 @@
 
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require('bcrypt');
 const addUser = require('.helpers/user_register');
-const
+const updateUser = require('.helpers/user_update');
 
-//GET info of current users
 module.exports = (db) => {
+
+  //GET info of current users
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users WHERE id = $1;`, )
+    const userId = req.session.userId;
+    db.query(`SELECT * FROM users WHERE id = $1;`,[userId])
       .then(data => {
         const users = data.rows;
         res.json({ users });
@@ -24,10 +27,38 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  //registering user
+  router.post("/addUser", (req, res) => {
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 12);
+    addUser(user, db)
+      .then(user => {
+        //req.session.userId = user.id;
+        res.send(user);
+      })
+      .catch(error =>
+        res.send("Error:", error));
+  });
+
+  //PUT or POST?
+  router.post("/:id/updateUser", (req, res) => {
+    const input = req.body;
+    const userId = req.params.id;
+
+    updateUser(userId, input, db)
+      .then(user => {
+        //req.session.userId = user.id;
+        res.send(user);
+      })
+      .catch(error =>
+        res.send("Error:", error));
+
+  });
+
   return router;
 };
 
-//POST - route for register new user -> i.e add user
-//front-end needs to create a signup button
+
 
 
